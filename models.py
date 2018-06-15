8e1d8d98f94a50bef5ec4a79081978a8bb8a8b58
gfed53 <gfederico7153@gmail.com>
2018-06-15 17:12:00 -0400

    models: created CachedList model to be used. view: in get_movies, using pipe character to treat genres list as OR rather than AND - in get_genres, added some rough code for integrating caching of genres list fetched from TMDB API.

--- models.py
+++ models.py
@@ -1,3 +1,14 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class CachedList(models.Model):
+  name = models.CharField(max_length=200)
+  list_data = models.TextField()
+  date_updated = models.DateTimeField()
+
+  def __str__(self):
+    return self.name
--- views.py
+++ views.py
@@ -2,6 +2,9 @@ import json
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest, JsonResponse
 
+from .models import CachedList
+from django.utils import timezone
+
 from .config import *
 
 import tmdbsimple as tmdb
@@ -26,7 +29,7 @@ def get_movies(request):
   my_kwargs = {}
 
   if 'genres' in params:
-    genres = ','.join(str(x) for x in params['genres'])
+    genres = '|'.join(str(x) for x in params['genres'])
     my_kwargs['with_genres'] = genres
     print('genres',genres)
 
@@ -47,11 +50,29 @@ def get_movies(request):
   discover = tmdb.Discover()
   response_data = discover.movie(**my_kwargs)
 
-
   return JsonResponse({'tmdb_results': response_data})
 
 def get_movie_genres(request):
   genre = tmdb.Genres()
+
+  # Check if there is a CacheList entry where name='genres' here
+  
+
   response_data = genre.movie_list()
+
+  # Basic implementation - create an entry into the CacheList model (after every API call while we're testing this out)
+
+
+  # response_data_str = str(response_data)
+  # cl = CachedList(
+  #   name='genres',
+  #   list_data=response_data_str,
+  #   date_updated=timezone.now()
+  # )
+
+  # cl.save()
+
+
+
   return JsonResponse(response_data)
 
